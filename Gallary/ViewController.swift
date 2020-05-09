90b50297b939dab5489eb0ab2c79a51b77bde2b9
khaled <>
2020-05-09 13:39:03 +0300

    How To Upload Image

---
+++ Gallary/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Gallary
+//
+//  Created by khaled on 5/9/20.
+//  Copyright © 2020 khaledHamdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  @IBOutlet weak var image: UIImageView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
+
+// MARK: - UIImagePickerController for camera and photo library
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  @IBAction func ac(_ sender: UIButton) {
+    let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+    alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+      self.openCamera()
+    }))
+    alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+      self.openGallery()
+    }))
+    alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+    self.present(alert, animated: true, completion: nil)
+  }
+  
+  func openCamera() {
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+      let imagePicker = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.sourceType = UIImagePickerController.SourceType.camera
+      imagePicker.allowsEditing = false
+      self.present(imagePicker, animated: true, completion: nil)
+    }
+    else {
+      let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+      self.present(alert, animated: true, completion: nil)
+    }
+  }
+  
+  func openGallery() {
+    if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+      let imagePicker = UIImagePickerController()
+      imagePicker.delegate = self
+      imagePicker.allowsEditing = true
+      imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+      self.present(imagePicker, animated: true, completion: nil)
+    }
+    else {
+      let alert  = UIAlertController(title: "Warning", message: "You don't have permission to access gallery.", preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+      self.present(alert, animated: true, completion: nil)
+    }
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let pickedImage = info[.originalImage] as? UIImage {
+      image.contentMode = .scaleToFill
+      image.image = pickedImage
+    }
+    picker.dismiss(animated: true, completion: nil)
+  }
+}
